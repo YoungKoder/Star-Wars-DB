c70b9343b5f651789c3cdff0634de94cea87cd98
Maksym Teslenko <maksteslenko924@gmail.com>
2020-02-10 16:52:37 +0200

    creating spinenr and seting up error-indicator

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SwapiService from "../../services/swapi-service";
 
+import Spinner from "../spinner";
+
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
@@ -10,30 +12,57 @@ export default class RandomPlanet extends Component {
     super();
 
     this.state = {
-      planet:{}
+      planet:{},
+      loading: true,
+      error:false
     }
     this.updatePlanet();
   }
 
   onPlanetLoaded = (planet)=>{
-    this.setState({planet});
+    this.setState({
+      planet,
+      loading: false
+    });
   }
 
+  onError = () =>{
+    this.setState({ 
+      error:true,
+      loading:false
+    })
+  }
   updatePlanet=()=>{
     const id = Math.floor(Math.random()*25)+2;
     this.swapi.getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.omError)
   }
   
   render() {
-    const {planet:{id, name, population, rotationPeriod,
-      diameter}} = this.state;
+    const {planet, loading} = this.state;
+    
+    const spinner = loading ? <Spinner/> : null;
+    const planetContent = !loading ? <PlanetView planet ={planet}/> : null
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
+        {spinner}
+        {planetContent}
+      </div>
+    );
+  }
+}
+const PlanetView = ({planet})=>{
+
+  const {id, name, population, rotationPeriod,
+    diameter} = planet;
+
+  return(
+    <>
+      <img className="planet-image"
              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
-    <h4>{name}</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
@@ -48,9 +77,7 @@ export default class RandomPlanet extends Component {
               <span>100</span>
             </li>
           </ul>
-        </div>
-      </div>
-
-    );
-  }
+        </div> 
+    </>
+  )
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./spinner.css";
+
+const Spinner= ()=>{
+    return(
+        <div className="loadingio-spinner-dual-ball-5dzkdotpf9r">
+            <div className="ldio-umc42i4u3tk">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+export default Spinner;
